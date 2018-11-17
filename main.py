37be5c7b906e7dc7fcd79713d6a1b1be44ef8e79
camjs2 <james@sadler.org.uk>
2018-11-17 20:57:05 +0000

    test

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 api = Api(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///persist.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(25), unique=True, nullable=False)
+    pwd_hash = db.Column(db.String(50), unique=False, nullable=True)
+    forename = db.Column(db.String(25), unique=False, nullable=False)
+    surname = db.Column(db.String(25), unique=False, nullable=False)
+    created_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __init__(self, username,  forename, surname):
+        self.username = username
+        self.forename = forename
+        self.surname = surname
+
+
+db.drop_all()
+db.create_all()
+user = User("camjs2", "james", "sadler")
+db.session.add(user)
+db.session.commit()
 
 
 class HelloWorld(Resource):
@@ -11,6 +38,7 @@ class HelloWorld(Resource):
 
 
 api.add_resource(HelloWorld, '/')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
